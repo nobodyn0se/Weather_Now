9ce2f855bfdcd03626d33387a7ee26018c71b508
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-11-03 19:47:36 +0530

    Light theme attributes set

--- lib/app_themes/app_themes.dart
+++ lib/app_themes/app_themes.dart
@@ -4,3 +4,19 @@ enum AppThemes {
   light,
   dark,
 }
+
+class AppThemeClass {
+  //theme map
+  static final themeData = {
+    AppThemes.light: ThemeData(
+      primaryColor: Colors.white,
+      scaffoldBackgroundColor: Colors.white,
+      textTheme: const TextTheme(
+        bodyText1: TextStyle(
+          color: Colors.black,
+        ),
+      ),
+      iconTheme: const IconThemeData(color: Colors.black),
+    ),
+  };
+}
