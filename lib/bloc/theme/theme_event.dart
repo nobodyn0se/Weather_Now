28d0d4f1086da9d50ce09418ae3f9ad755dfbb0a
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-11-04 23:41:00 +0530

    Theme Event base class added

---
+++ lib/bloc/theme/theme_event.dart
@@ -0,0 +1,7 @@
+import 'package:bloc_app/app_themes/app_themes.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+
+@immutable
+abstract class ThemeEvent extends Equatable {}
+
