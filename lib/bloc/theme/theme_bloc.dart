b1e73b510e4ffdd77e08fc16abffd705a5b9cadc
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-11-04 23:44:11 +0530

    Theme Bloc class  added

---
+++ lib/bloc/theme/theme_bloc.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../app_themes/app_themes.dart';
+
+import './theme_event.dart';
+import './theme_state.dart';
+
+class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
+}
