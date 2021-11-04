ebfe6789867672e7207fc4a00b4a611fab4cc870
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-11-04 23:50:13 +0530

    Necessary imports done in main_page.dart

--- lib/widgets/main_page.dart
+++ lib/widgets/main_page.dart
@@ -1,4 +1,11 @@
+import 'package:bloc_app/app_themes/app_themes.dart';
+import 'package:bloc_app/bloc/theme/theme_event.dart';
+import 'package:bloc_app/bloc/theme/theme_state.dart';
 import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/theme/theme_bloc.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
