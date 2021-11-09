bc273a1e1fda48a7dce818ddd83dbbf72c46d410
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-11-09 17:53:32 +0530

    WeatherRepository initialized and passed to MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,40 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
 
 import '../bloc/theme/theme_state.dart';
 import './bloc/theme/theme_bloc.dart';
 
+import './bloc/weather/weather_bloc.dart';
+
+import './repository/weather_repository.dart';
+import './weather_data/weather_data_layer.dart';
+
 import './widgets/main_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  final WeatherRepository weatherRepository = WeatherRepository(
+    weatherDataClient: WeatherDataClient(
+      httpClient: http.Client(),
+    ),
+  );
+  runApp(MyApp(
+    weatherRepository: weatherRepository,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key, required this.weatherRepository}) : super(key: key);
+  final WeatherRepository weatherRepository;
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
         BlocProvider<ThemeBloc>(
-      create: (context) => ThemeBloc(),
+          create: (context) => ThemeBloc(),
         ),
         BlocProvider<WeatherBloc>(
             create: (context) =>
