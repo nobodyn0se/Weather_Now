71e68f8516a087323bc5c294e4a4fd69f80907b0
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-11-09 20:32:15 +0530

    Build method defined for WeatherPage with BlocBuilder

--- lib/widgets/weather_page.dart
+++ lib/widgets/weather_page.dart
@@ -15,4 +15,46 @@ class WeatherPage extends StatefulWidget {
 
 class _WeatherPageState extends State<WeatherPage> {
   late String city;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              BlocProvider.of<ThemeBloc>(context).add(ThemeToggled());
+            },
+            icon: const Icon(Icons.lightbulb_rounded),
+          )
+        ],
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      extendBodyBehindAppBar: true,
+      body: Center(
+        child: BlocBuilder<WeatherBloc, WeatherState>(
+            builder: (context, weatherState) {
+          if (weatherState is WeatherNull) {
+            return const Text('No data to display');
+          } else if (weatherState is WeatherFetching) {
+            return const CircularProgressIndicator();
+          } else if (weatherState is WeatherFetched &&
+              weatherState.weather.currTemp != null) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(city),
+                Text('Current: ${weatherState.weather.currTemp}'),
+                Text('Low: ${weatherState.weather.minTemp}'),
+                Text('High: ${weatherState.weather.maxTemp}'),
+                Text('Humidity: ${weatherState.weather.humidity}%')
+              ],
+            );
+          } else {
+            return const Text('Error while fetching weather');
+          }
+        }),
+      ),
+    );
+  }
 }
