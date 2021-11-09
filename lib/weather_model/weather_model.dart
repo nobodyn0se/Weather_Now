ba986f53df0c8ee71bee284f133ad5689b759f7e
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-11-09 15:34:31 +0530

    Weather Model attributes turned nullable

--- lib/weather_model/weather_model.dart
+++ lib/weather_model/weather_model.dart
@@ -1,24 +1,24 @@
 class WeatherModel {
-  final String weatherCondition;
-  final double minTemp;
-  final double maxTemp;
-  final double currTemp;
-  final double windSpeed;
-  final double windDirection;
-  final double airPressure;
-  final double humidity;
-  final double visibility;
+  final String? weatherCondition;
+  final double? minTemp;
+  final double? maxTemp;
+  final double? currTemp;
+  final double? windSpeed;
+  final double? windDirection;
+  final double? airPressure;
+  final double? humidity;
+  final double? visibility;
 
   WeatherModel(
-      {required this.airPressure,
-      required this.currTemp,
-      required this.humidity,
-      required this.maxTemp,
-      required this.minTemp,
-      required this.visibility,
-      required this.weatherCondition,
-      required this.windDirection,
-      required this.windSpeed});
+      {this.airPressure,
+      this.currTemp,
+      this.humidity,
+      this.maxTemp,
+      this.minTemp,
+      this.visibility,
+      this.weatherCondition,
+      this.windDirection,
+      this.windSpeed});
 
   factory WeatherModel.fromJson(dynamic json) {
     final initWeather = json['consolidated_weather'][0];
