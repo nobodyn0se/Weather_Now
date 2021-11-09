342675b5efd778306ff9941308d431f1150f767c
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-11-09 15:22:41 +0530

    getLocationID method added to data layer

--- lib/weather_data/weather_data_layer.dart
+++ lib/weather_data/weather_data_layer.dart
@@ -7,4 +7,19 @@ class WeatherDataClient {
   final http.Client httpClient;
 
   WeatherDataClient({required this.httpClient});
+
+  Future<int> getLocID(String city) async {
+    final locURL = Uri.parse('$_mainURL/api/location/search/?query=$city');
+    try {
+      final locResponse = await httpClient.get(locURL);
+      if (locResponse.statusCode == 200) {
+        final locResult = jsonDecode(locResponse.body) as List;
+        return (locResult.first)['woeid'];
+      }
+    } catch (e) {
+      print('Error occured in data_layer/getLocID: ${e.toString()}');
+      return -1;
+    }
+    return -1;
+  }
 }
