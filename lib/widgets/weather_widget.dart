e15a9d83358d027dcfde4d8ee7103752bb67240d
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-11-12 17:14:51 +0530

    Overall page layout defined with a Column and SizedBox

--- lib/widgets/weather_widget.dart
+++ lib/widgets/weather_widget.dart
@@ -14,68 +14,66 @@ class WeatherWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      //color: Colors.red,
-      width: 300,
-      height: 500,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(
-            width: double.maxFinite,
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Text(
-                  city,
-                  style: Theme.of(context).textTheme.headline5,
-                  textAlign: TextAlign.center,
-                ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(
+          width: double.maxFinite,
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
+                city,
+                style: Theme.of(context).textTheme.headline5,
+                textAlign: TextAlign.center,
               ),
             ),
           ),
-          IntrinsicHeight(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Card(
+        ),
+        SizedBox(
+          width: 300,
+          height: 250,
+          child: Row(
+            children: [
+              Expanded(
+                child: Card(
                   color: Colors.transparent,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Text(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text(
                         '${weather.currTemp!.toStringAsFixed(2)} \u2103',
                         style: Theme.of(context).textTheme.headline3,
                       ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: <Widget>[
-                        Text('Low: ${weather.minTemp!.toStringAsFixed(2)} C'),
-                        Text(
+                          Text('Low: ${weather.minTemp!.toStringAsFixed(2)} C'),
+                          Text(
                               'High: ${weather.maxTemp!.toStringAsFixed(2)} C'),
                         ],
                       ),
-                      ],
-                    ),
+                    ],
                   ),
                 ),
-                Stack(
-                  alignment: Alignment.bottomCenter,
-                  children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.symmetric(vertical: 3),
-                      height: 250,
-                      width: 30,
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        border: Border.all(
+              ),
+              Stack(
+                alignment: Alignment.bottomCenter,
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 3),
+                    height: 250,
+                    width: 30,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      border: Border.all(
                           color: Theme.of(context).textTheme.bodyText1!.color!),
-                      ),
                     ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(vertical: 4),
-                      height: weather.humidity! * 0.01 * 250,
-                      width: 27,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 4),
+                    height: weather.humidity! * 0.01 * 250,
+                    width: 27,
                     decoration: BoxDecoration(
                         color: Theme.of(context).secondaryHeaderColor),
                   ),
@@ -85,14 +83,15 @@ class WeatherWidget extends StatelessWidget {
                       '${weather.humidity.toString()}\n%',
                       textAlign: TextAlign.center,
                     ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        Text('Air Pressure: ${weather.airPressure!.toInt()} hPa'),
+        Text(weather.weatherCondition!),
+      ],
     );
   }
 }
