88f75e44255ff6e893e917c21dcb979d9e7e6a4d
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-11-12 15:44:25 +0530

    Weather widget extracted and moved for readability

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -46,79 +46,9 @@ class _WeatherPageState extends State<WeatherPage> {
             return const CircularProgressIndicator();
           } else if (weatherState is WeatherFetched &&
               weatherState.weather.currTemp != null) {
-            return SizedBox(
-              //color: Colors.red,
-              width: 300,
-              height: 500,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    width: double.maxFinite,
-                    child: Card(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: Text(
-                          city,
-                          style: Theme.of(context).textTheme.headline5,
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                  ),
-                  IntrinsicHeight(
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Card(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                Text(
-                                    'Current: ${weatherState.weather.currTemp!.toStringAsFixed(2)} C'),
-                                Text(
-                                    'Low: ${weatherState.weather.minTemp!.toStringAsFixed(2)} C'),
-                                Text(
-                                    'High: ${weatherState.weather.maxTemp!.toStringAsFixed(2)} C'),
-                                Text(
-                                    'Humidity: ${weatherState.weather.humidity}%'),
-                                Text(
-                                    'Air Pressure: ${weatherState.weather.airPressure!.toInt()} hPa'),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Stack(
-                          alignment: Alignment.bottomCenter,
-                          children: <Widget>[
-                            Container(
-                              margin: const EdgeInsets.symmetric(vertical: 3),
-                              height: 250,
-                              width: 30,
-                              decoration: BoxDecoration(
-                                color: Theme.of(context).primaryColor,
-                                border: Border.all(
-                                    color: Theme.of(context)
-                                        .textTheme
-                                        .bodyText1!
-                                        .color!),
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.symmetric(vertical: 4),
-                              height:
-                                  weatherState.weather.humidity! * 0.01 * 250,
-                              width: 27,
-                              decoration:
-                                  BoxDecoration(color: Colors.lightBlue[100]),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+            return WeatherWidget(
+              city: city,
+              weather: weatherState.weather,
             );
           } else if (weatherState is WeatherException) {
             return Text(weatherState.errorMessage);
---
+++ lib/widgets/weather_widget.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+import '../weather_model/weather_model.dart';
+
+class WeatherWidget extends StatelessWidget {
+  const WeatherWidget({
+    Key? key,
+    required this.city,
+    required this.weather,
+  }) : super(key: key);
+
+  final String city;
+  final WeatherModel weather;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      //color: Colors.red,
+      width: 300,
+      height: 500,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            width: double.maxFinite,
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                  city,
+                  style: Theme.of(context).textTheme.headline5,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+          IntrinsicHeight(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Card(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Text(
+                            'Current: ${weather.currTemp!.toStringAsFixed(2)} C'),
+                        Text('Low: ${weather.minTemp!.toStringAsFixed(2)} C'),
+                        Text('High: ${weather.maxTemp!.toStringAsFixed(2)} C'),
+                        Text('Humidity: ${weather.humidity}%'),
+                        Text(
+                            'Air Pressure: ${weather.airPressure!.toInt()} hPa'),
+                      ],
+                    ),
+                  ),
+                ),
+                Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 3),
+                      height: 250,
+                      width: 30,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        border: Border.all(
+                            color:
+                                Theme.of(context).textTheme.bodyText1!.color!),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 4),
+                      height: weather.humidity! * 0.01 * 250,
+                      width: 27,
+                      decoration: BoxDecoration(color: Colors.lightBlue[100]),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
