a2e769ece92c977415da147678bc258056260c2d
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-11-15 13:50:03 +0530

    Height and width constraints changed for the temp display

--- lib/widgets/weather_widget.dart
+++ lib/widgets/weather_widget.dart
@@ -63,60 +63,56 @@ class WeatherWidget extends StatelessWidget {
             endIndent: 0.1 * screenWidth,
           ),
           SizedBox(
-            width: 300,
-            height: 250,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Card(
-                    elevation: 0,
-                    color: Theme.of(context).scaffoldBackgroundColor,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        createTempMeter(context),
-                        Text(
-                          '${weather.currTemp!.toStringAsFixed(2)} \u2103',
-                          style: Theme.of(context).textTheme.headline3,
-                        ),
-                    // Row(
-                    //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    //   children: <Widget>[
-                    //     Card(
-                    //       color: Theme.of(context).scaffoldBackgroundColor,
-                    //       child: Padding(
-                    //         padding: const EdgeInsets.all(8.0),
-                    //         child: Text(
-                    //             'Low: ${weather.minTemp!.toStringAsFixed(2)} C'),
-                    //       ),
-                    //       shape: const RoundedRectangleBorder(
-                    //         side: BorderSide(
-                    //           color: Colors.lightBlue,
-                    //         ),
-                    //         borderRadius:
-                    //             BorderRadius.all(Radius.circular(4)),
-                    //       ),
-                    //     ),
-                    //     Card(
-                    //       color: Theme.of(context).scaffoldBackgroundColor,
-                    //       child: Padding(
-                    //         padding: const EdgeInsets.all(8.0),
-                    //         child: Text(
-                    //             'High: ${weather.maxTemp!.toStringAsFixed(2)} C'),
-                    //       ),
-                    //       shape: const RoundedRectangleBorder(
-                    //         side: BorderSide(
-                    //           color: Colors.red,
-                    //         ),
-                    //         borderRadius:
-                    //             BorderRadius.all(Radius.circular(4)),
-                    //       ),
-                    //     ),
-                    //   ],
-                    // ),
-                      ],
-                    ),
+            width: 350,
+            height: 200,
+            child: Card(
+              elevation: 0,
+              color: Theme.of(context).scaffoldBackgroundColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  createTempMeter(context),
+                  Text(
+                    '${weather.currTemp!.toStringAsFixed(2)} \u2103',
+                    style: Theme.of(context).textTheme.headline3,
                   ),
+                  // Row(
+                  //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  //   children: <Widget>[
+                  //     Card(
+                  //       color: Theme.of(context).scaffoldBackgroundColor,
+                  //       child: Padding(
+                  //         padding: const EdgeInsets.all(8.0),
+                  //         child: Text(
+                  //             'Low: ${weather.minTemp!.toStringAsFixed(2)} C'),
+                  //       ),
+                  //       shape: const RoundedRectangleBorder(
+                  //         side: BorderSide(
+                  //           color: Colors.lightBlue,
+                  //         ),
+                  //         borderRadius:
+                  //             BorderRadius.all(Radius.circular(4)),
+                  //       ),
+                  //     ),
+                  //     Card(
+                  //       color: Theme.of(context).scaffoldBackgroundColor,
+                  //       child: Padding(
+                  //         padding: const EdgeInsets.all(8.0),
+                  //         child: Text(
+                  //             'High: ${weather.maxTemp!.toStringAsFixed(2)} C'),
+                  //       ),
+                  //       shape: const RoundedRectangleBorder(
+                  //         side: BorderSide(
+                  //           color: Colors.red,
+                  //         ),
+                  //         borderRadius:
+                  //             BorderRadius.all(Radius.circular(4)),
+                  //       ),
+                  //     ),
+                  //   ],
+                  // ),
+                ],
+              ),
             ),
           ),
           Divider(
@@ -174,34 +170,34 @@ class WeatherWidget extends StatelessWidget {
           weather.minTemp!.toStringAsFixed(1),
         ),
         Stack(
-      alignment: Alignment.centerRight,
-      children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-                color: Theme.of(context).textTheme.bodyText1!.color!),
-            gradient: const LinearGradient(
-              colors: [
-                Colors.white,
-                Colors.lightBlue,
-                Colors.blue,
-                Colors.green,
-                Colors.yellow,
-                Colors.orange,
-                Colors.deepOrange,
-                Colors.red,
-                Colors.brown,
-              ],
-              stops: [0.05, 0.2, 0.3, 0.45, 0.6, 0.65, 0.7, 0.75, 0.8],
+          alignment: Alignment.centerRight,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                    color: Theme.of(context).textTheme.bodyText1!.color!),
+                gradient: const LinearGradient(
+                  colors: [
+                    Colors.white,
+                    Colors.lightBlue,
+                    Colors.blue,
+                    Colors.green,
+                    Colors.yellow,
+                    Colors.orange,
+                    Colors.deepOrange,
+                    Colors.red,
+                    Colors.brown,
+                  ],
+                  stops: [0.05, 0.2, 0.3, 0.45, 0.6, 0.65, 0.7, 0.75, 0.8],
+                ),
+              ),
+              height: 10,
+              width: 240,
             ),
-          ),
-          height: 10,
-          width: 240,
-        ),
-        Container(
-          color: Theme.of(context).primaryColor,
-          height: 10,
-          width: (240 - (weather.currTemp! + 50) * 2),
+            Container(
+              color: Theme.of(context).primaryColor,
+              height: 10,
+              width: (240 - (weather.currTemp! + 50) * 2),
             ),
           ],
         ),
